58d29fbc4b4be85b64d11ed1008fa4ffffbaccd0
ColonelBucket8 <freedomterran89@gmail.com>
2022-10-01 20:25:47 +0800

    added flexible html content functions

--- hello.hs
+++ hello.hs
@@ -1,5 +1,15 @@
 main = putStrLn myhtml
-myhtml = wrapHtml "Hello, world"
+
+myhtml = makeHtml "This is a test" "Hello world"
 
 wrapHtml content = "<html><body>" <> content <> "</body></html>"
 
+html_ content = "<html>" <> content <> "</html>"
+
+body_ content = "<body>" <> content <> "</body>"
+
+head_ content = "<head>" <> content <> "</head>"
+
+title_ content = "<title>" <> content <> "</title>"
+
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
