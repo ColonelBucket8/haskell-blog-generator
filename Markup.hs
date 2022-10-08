d9182d40f65155930a3674a1a6d66fa2e0eaf66d
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-10-08 17:57:31 +0800

    added markup module and data structure

---
+++ Markup.hs
@@ -0,0 +1,54 @@
+module Markup 
+  ( Document
+  , Structure
+  )
+where
+
+import Numeric.Natural
+
+type Document = [Structure]
+
+data Structure 
+  = Heading Natural String
+  | Paragraph String
+  | UnorderedList [String]
+  | OrderedList [String]
+  | CodeBlock [String]
+
+ex1 :: Document
+ex1 = [Paragraph "Hello World"]
+
+ex2 :: Document
+ex2 = [Heading 1 "Welcome", Paragraph "To this tutorial about Haskell"]
+
+ex3 :: Document
+ex3 = 
+  [ Paragraph "Remember that multiple lines with no separation are grouped together to a single paragraph but list items remain separate."
+  , UnorderedList ["Item 1 of a list", "Item 2 of a list"] 
+  ]
+
+ex4 :: Document
+ex4 = 
+  [ Heading 1 "Compiling programs with ghc"
+  , Paragraph "Running ghc invokes the Glasgow Haskell Compiler (GHC), and can be used to compile Haskell modules and programs into native executables and libraries."
+  , Paragraph "Create a new Haskell source file named hello.hs, and write the following code in it:"
+  , CodeBlock [ "main = putStrLn \"Hello, Haskell!\""]
+  , Paragraph "Now, we can compile the program by invoking ghc with the file name:" 
+  , CodeBlock 
+    [ "-> ghc hello.hs"
+    , "[1 0f 1] Compiling Main"  
+    , "Linking hello ..."
+    ]
+  , Paragraph "GHC created the following files:"
+  , UnorderedList
+    [ "hello.hi - Haskell interface file"
+    , "hello.o - Object file, the output of the compiler before linking"
+    , "hello (or hello.exe on Microsoft Windows) - A native runnable executable."
+    ]
+  , Paragraph "GHC will produce an executable when the source file satisfies both conditions:"
+  , OrderedList
+    [ "Defines the main function in the source file"
+    , "Defines the module name to be Main, or does not have a module declaration"
+    ]
+  , Paragraph "Otherwise, it will only produce the .o and .hi files."
+  ]
