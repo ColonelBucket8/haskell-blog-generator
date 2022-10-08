37a043a2ad24fe6cc1c893f537a584964dd8dded
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-10-08 17:32:51 +0800

    added unordered and ordered list

--- Html.hs
+++ Html.hs
@@ -5,61 +5,10 @@ module Html
   , html_
   , p_
   , h1_
+  , ul_
   , append_
   , render
   )
   where 
   
-newtype Html = Html String
-
-newtype Structure = Structure String
-
-type Title = String
-
-html_ :: Title -> Structure -> Html
-html_ title content =
-  Html 
-    ( el "html"
-      ( el "head" (el "title" (escape title))
-        <> el "body" (getStructureString content)
-      ) 
-    )
-
-p_ :: String -> Structure
-p_ = Structure . el "p" . escape
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1" . escape
-
-el :: String -> String -> String
-el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-
-append_ :: Structure -> Structure -> Structure
-append_ (Structure a) (Structure b) = 
-  Structure (a <> b)
-
-render :: Html -> String
-render html = 
-  case html of
-    Html str -> str
-
-getStructureString :: Structure -> String
-getStructureString struct =
-  case struct of 
-    Structure str -> str
-
-escape :: String -> String
-escape = 
-  let 
-    escapeChar c =
-      case c of
-        '<' -> "&lt;"
-        '>' -> "&gt;"
-        '&' -> "&amp;"
-        '"' -> "&quot:"
-        '\'' -> "&#39;"
-        _ -> [c]
-  in 
-    concat .map escapeChar
-
+  import Html.Internal
